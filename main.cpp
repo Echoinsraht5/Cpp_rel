b804a37221764af4f8987db5b7b702f77399819f
Spike <mcgregor20000125@gmail.com>
2022-08-05 17:06:03 +0800

    update acwing

--- main.cpp
+++ main.cpp
@@ -1,2 +1,196 @@
 // 111
-// 222
+// 222  
+
+#include<bits/stdc++.h>
+#include<iostream>
+
+
+using namespace std;
+
+int main ()
+{
+   double a  = 100.01;
+   int b  = (int)a;
+   int c = (a - b) * 1000;
+   cout << c << endl;
+   
+   return 0;
+
+}
+
+
+
+
+// // int main()
+// // {
+// //     // cout << "hello world" << endl;
+// //     int a, b;
+// //     a = 2;
+// //     b = a % 4;
+// //     cout << b << endl;
+// //     return 0;
+// // }
+
+// // int main()
+// // {
+// //     double a, b, c;
+// //     cin >> a >> b >> c;
+// //     printf("MEDIA = %.1f", (a*2 + b*3+ c*5) / 10 );
+    
+    
+// //     return 0;
+    
+// // }
+
+
+// // int main()
+// // {
+// //     char a[10];
+// //     a = (char)'AFHIOQEFOQ';
+// //     double b = 424.52;
+// //     double c = 243.61;
+// //     cin >> a >> b >> c;
+    
+// //     printf("TOTAL = R$ %.2lf", b + c * 0.15);
+    
+// //     return 0;
+// // }
+
+// // int main()
+// // {
+// //    int a, b, c, d, e, f;
+// //    cin >> a >> b >> c;
+// //    d = (a + b + abs(a - b)) / 2;
+// //    e = (a + c + abs(a - c)) / 2;
+// //    f = (b + c + abs(b - c)) / 2;
+// //    if (d == a && e == a)
+// //    {
+// //     cout << a << " eh o maior" << endl;
+// //    }
+// //    else if (d == a && e == c)
+// //    {
+// //     cout << c << " eh o maior" << endl;
+// //    }
+// //    else if (d == b && e == a)
+// //    {
+// //     cout << b << " eh o maior" << endl;
+// //    }
+// //    else if (d == b && e == c)
+// //    {
+// //     cout << f << " eh o maior" << endl;
+// //    }
+   
+// //    return 0;
+    
+// // }
+
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+// //     float n, n2;
+// //     int n1, i;
+// //     cin >> n;
+// //     n1 = (int)n;
+// //     n2 = n - n1;
+// //     for (i = n1 / 100)
+// //     {
+// //         cout << i << " nota(s) de R$ 100.00" << endl;
+// //         n1 = n1 % 100;
+// //         if (n1 < 100)
+// //         {
+// //             for( i = n1 / 50)
+// //             {
+// //                 cout << i << " nota(s) de R$ 50.00" << endl;
+// //                 n1 = n1 % 50;
+// //                 if (n1 < 50)
+// //                 {
+// //                     for( i = n1 / 20)
+// //                     {
+// //                        cout << i << " nota(s) de R$ 20.00" << endl; 
+// //                        n1 = n1 % 20;
+// //                        if (n1 < 20)
+// //                        {
+// //                           for( i = n1 / 10)
+// //                           {
+// //                             cout << i << " nota(s) de R$ 10.00" << endl; 
+// //                             n1 = n1 % 10;
+// //                             if (n1 < 10)
+// //                             {
+// //                                for ( i = n1 / 5)
+// //                                {
+// //                                  cout << i << " nota(s) de R$ 5.00" << endl;  
+// //                                  n1 = n1 % 5;
+// //                                  if (n1 < 5)
+// //                                 {
+// //                                   for ( i = n1 / 2)
+// //                                   {
+// //                                     cout << i << " nota(s) de R$ 2.00" << endl; 
+// //                                     n1 = n1 % 2;
+// //                                     if (n1 < 2)
+// //                                   {
+// //                                      for ( i = n1 / 1)
+// //                                      {
+// //                                        cout << i << " nota(s) de R$ 1.00" << endl;
+// //                                        n1 = n1 % 1;
+// //                                        if (n1 < 1)
+// //                                        {
+// //                                           for ( i = n2 / 0.5)
+// //                                           {
+// //                                             cout << i << " nota(s) de R$ 0.5" << endl; 
+// //                                             n2 -= 0.5;
+// //                                             if (n2 < 0.5)
+// //                                             {
+// //                                                 for ( i = n2 / 0.25)
+// //                                                 {
+// //                                                     cout << i << " nota(s) de R$ 0.25" << endl;
+// //                                                     n2 -= 0.25;
+// //                                                     if (n2 < 0.25)
+// //                                                     {
+// //                                                       for ( i = n2 / 0.1) 
+// //                                                       {
+// //                                                         cout << i << " nota(s) de R$ 0.1" << endl;
+// //                                                         n2 -= 0.1;
+// //                                                         if (n2 < 0.1) 
+// //                                                         {
+// //                                                            for ( i = n2 / 0.05)
+// //                                                            {
+// //                                                               cout << i << " nota(s) de R$ 0.05" << endl;
+// //                                                               n2 -= 0.05;
+// //                                                               if (n2 < 0.05)
+// //                                                               {
+// //                                                                  for ( i = n2 / 0.01)
+// //                                                                  {
+// //                                                                      cout << i << " nota(s) de R$ 0.01" << endl;
+// //                                                                      break;
+// //                                                                  }
+// //                                                               }
+// //                                                            }
+// //                                                         }
+// //                                                       }
+// //                                                     }
+// //                                                 }
+// //                                             }
+// //                                           }
+// //                                        }
+// //                                      }
+                                      
+// //                                   }
+// //                                 }
+// //                                }
+// //                             }
+// //                           }
+// //                        }
+// //                     }
+// //                 }
+// //             }
+// //         }
+// //     }
+ 
+  
+  
+// //   return 0;  
+// }
