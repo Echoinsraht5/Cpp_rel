57adfe8385491ee9d223fdec3e154fa4e5912c99
Spike <mcgregor20000125@gmail.com>
2022-08-05 18:10:07 +0800

    update test

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b = 2125;
+    double c = 15.1153;
+    
+    printf("%-5d!\n", a);
+    printf("%15d!\n", b);
+    printf("%08.3lf!\n", c);
+    
+    return 0;
+}
