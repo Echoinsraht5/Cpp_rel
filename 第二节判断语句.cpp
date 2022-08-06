1e1bcbd0c3763ff87114bccc20cc984a2c60e522
Spike <mcgregor20000125@gmail.com>
2022-08-06 13:44:31 +0800

    update acwing

--- 第二节判断语句.cpp
+++ 第二节判断语句.cpp
@@ -43,11 +43,45 @@ int main()
     // else cout << 'D' << endl;
 
 
-    int a;
-    cin >> a;
-    if (a % 400 == 0 || a % 100 != 0 && a % 4 == 0) cout << "yes" << endl;
-    else cout << "no" << endl;
+    // int a;
+    // cin >> a;
+    // if (a % 400 == 0 || a % 100 != 0 && a % 4 == 0) cout << "yes" << endl;
+    // else cout << "no" << endl;
 
-    return 0;
+
+
+
+    double a, b, c;
+    a = 1.2;
+    b = 2.3;
+    c = 2.3;
     
+    cin >> a >> b >> c;
+    if (a >= b + c || a + c >= b || b + c >= a)
+    {
+        if (a * a == b * b + c * c || b * b == a * a + c * c || c * c ==  a * a + b * b) 
+        {
+            if (a == b && b == c) cout << "TRIANGULO RETANGULO" << "\n" << "TRIANGULO EQUILATERO" << endl;
+            else if (a == b && a != c || a == c && a != b || b == c && b != a) cout << "TRIANGULO RETANGULO" << "\n" << "TRIANGULO ISOSCELES" << endl;
+            else cout << "TRIANGULO RETANGULO" << endl;
+        }    
+        else if (a * a > b * b + c * c || b * b > a * a + c * c || c * c > a * a + b * b) 
+        {
+            if (a == b && b == c) cout << "TRIANGULO OBTUSANGULO" << "\n" << "TRIANGULO EQUILATERO" << endl;
+            else if (a == b && a != c || a == c && a != b || b == c && b != a) cout << "TRIANGULO RETANGULO" << "\n"  << "TRIANGULO ISOSCELES" << endl; 
+            else cout << "TRIANGULO OBTUSANGULO" << endl;
+        }
+        else if (a * a < b * b + c * c || b * b < a * a + c * c || c * c < a * a + b * b) 
+        {
+            if (a == b && b == c) cout << "TRIANGULO ACUTANGULO" << "\n" << "TRIANGULO EQUILATERO" << endl;
+            else if (a == b && a != c || a == c && a != b || b == c && b != a) cout << "TRIANGULO RETANGULO" << "\n"  << "TRIANGULO ISOSCELES" << endl; 
+            else cout << "TRIANGULO ACUTANGULO" << endl;
+        }
+        
+        
+    }
+    else cout << "NAO FORMA TRIANGULO" << endl;
+       
+    return 0;
+  
 }
