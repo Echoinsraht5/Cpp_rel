bcb4ce148c8b7cb4ad6d539e026515d58a6e912c
Spike <mcgregor20000125@gmail.com>
2022-08-17 17:12:34 +0800

    update acwing

--- test.cpp
+++ test.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 #include <typeinfo>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
 
 
 using namespace std;
@@ -7,16 +12,31 @@ using namespace std;
 int main()
 {
     // int a = 2;
-    int n = 22;
-    char s = 's';
-    // int b = 2125;
+    // char s = 's';
+    int a[] = {1, 1, 2, 2, 3, 3, 4};
+    int *p  = a;
+    int *q  = unique(a, a + 7);
+    int m  = unique(a, a + 7) - a;
+    cout << m << ' ' << *p << ' ' << *q << endl;
+    // cout << p << ' ' << p + 1 << ' ' << q << ' ' << m << endl;
+    for (int i = 0; i < m; i ++ ) cout << a[i] << ' ';
+    // // int b = 2125;
     // double c = 15.1153;
     
     // printf("%-5d!\n", a);
     // printf("%15d!\n", b);
     // printf("%08.3lf!\n", c);
-    cout << sizeof(n) << endl;
+    
     // if (n) cout << a << endl;
     
+    // queue<string> s;
+
+    // s.push("hello");
+    // s.push("hello1");
+    // s.push("hello2");
+    // s.pop();
+
+    // cout << s.front() << endl;
+
     return 0;
 }
--- 链表&STL相关.cpp
+++ 链表&STL相关.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -31,7 +34,7 @@ int main()
     priority_queue<int> p; // 大根堆
     priority_queue<int, vector<int>, greater <int>> o; // 小根堆
 
-    // 注意：大根堆中引用结构体需要定义小于号，小根堆中引用结构体需定义大于号
+    // 注意：大根堆中引用结构体需要重载小于号，小根堆中引用结构体需重载大于号
 
     struct Rec
     {
@@ -45,6 +48,15 @@ int main()
     priority_queue<Rec, vector<Rec>, greater<Rec>> d;
     
     d.push({1, 2});
+
+
+    queue<string> s;
+    s.push("hello");
+    s.push("hello1");
+    s.push("hello2");
+    s.pop();
+
+    cout << s.front() << endl;
 }
         
 
